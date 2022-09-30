fa72ad0635676d97d04941b38f180e418f132234
Anton Salameh <34179565+tsalameh512@users.noreply.github.com>
2022-09-30 15:55:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import pandas as pd
+from time import sleep
+import os
     
 def main():
-    datas = pd.read_csv('https://www.nyse.com/api/trade-halts/current/download')
-    print(datas)
+    while True == True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        datas = pd.read_csv('https://www.nyse.com/api/trade-halts/current/download')
+        print(datas)
+        sleep(10)
 
 main()
