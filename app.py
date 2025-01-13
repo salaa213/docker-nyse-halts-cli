1aa34e10b00198b67acc23613107cff99059bbe0
Anton Salameh <34179565+tsalameh512@users.noreply.github.com>
2025-01-13 12:06:52 -0800

    Update app.py
    Cleaned up output in CLI

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 import pandas as pd
 from time import sleep
 import os
-    
-def main():
-    while True == True:
+
+def fetch_csv_from_nyse():
+    try:
+        # Adjust Pandas options to prevent line breaks
+        pd.set_option('display.max_rows', None)  # Show all rows
+        pd.set_option('display.max_columns', None)  # Show all columns
+        pd.set_option('display.width', 1000)  # Set terminal width to 1000 characters
+
         os.system('cls' if os.name == 'nt' else 'clear')
-        datas = pd.read_csv('https://www.nyse.com/api/trade-halts/current/download')
-        print(datas)
+        url = 'https://www.nyse.com/api/trade-halts/current/download'
+        data = pd.read_csv(url)
+        print(data)
+    except Exception as e:
+        print(f"Error fetching data: {e}")
+
+def main():
+    """
+    Main loop to repeatedly fetch and display data every 10 seconds.
+    """
+    while True:
+        fetch_csv_from_nyse()
         sleep(10)
 
 main()
